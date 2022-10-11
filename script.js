952027da6a718151c05b14f11859f019ee80b901
Alice Shao <a1ice_shao@yahoo.com>
2022-10-11 01:10:31 -0400

    fixed api function and added sections in html to place future weather data

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 var searchEl = document.getElementById("search-btn");
-var cityEl = document.getElementById("city-name");
+var cityEl = document.getElementById("city-input");
 
 var APIKey = "43dce1cbede3db860e66e5fee0faaf41";
 
 
 
-function getApi() {
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityEl.value() + "&appid=" + APIKey;
+function getApi(event) {
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityEl.value + "&units=imperial" + "&appid=" + APIKey;
+    event.preventDefault();
 
     fetch(queryURL)
       .then(function (response) {
