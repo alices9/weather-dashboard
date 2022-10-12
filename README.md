30e3a6e66c453fb0e8447dd838ce078357cc7e63
Alice Shao <a1ice_shao@yahoo.com>
2022-10-11 21:12:18 -0400

    added README and added weather icons to the dashboard

---
+++ README.md
@@ -0,0 +1,22 @@
+# <Weather Dashboard>
+
+## Description
+
+This site will allow the user to search for a city and obtain the current weather conditions as well as the future 5 day forecast.
+
+## Installation
+
+Site can be viewed at https://alices9.github.io/weather-dashboard/
+
+## Usage
+
+User can input a city name and hit the search button. The current weather as wel as the weather over the next 5 days will populate to the right. User can view the temperatures, wind speeds, and humidity levels.
+
+<img src="./images/weatherdash.png" alt="screenshot of weather dashboard application">
+
+## Credits
+
+This page was created with
+Bootstrap https://getbootstrap.com/
+Moment.js https://momentjs.com/
+OpenWeather API https://openweathermap.org/
--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 var searchEl = document.getElementById("search-btn");
 var cityEl = document.getElementById("city-input");
-var todaysWeatherEl = document.getElementById("current-city-name");
+var historyEl = document.getElementById("past-searches");
+var currentCityEl = document.getElementById("current-city-name");
+var currentIconEl = document.getElementById("current-icon");
 var currentTempEl = document.getElementById("current-temp");
 var currentWindEl = document.getElementById("current-wind");
 var currentHumEl = document.getElementById("current-hum");
+var forecastHeadingEl = document.getElementById("future-forecast");
 var futureEl = document.getElementById("future-weather");
 
 var APIKey = "43dce1cbede3db860e66e5fee0faaf41";
@@ -12,15 +15,16 @@ var APIKey = "43dce1cbede3db860e66e5fee0faaf41";
 // getting today's weather
 function todaysWeather () {
     var weatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityEl.value + "&units=imperial" + "&appid=" + APIKey;
-    // event.preventDefault();
 
     fetch(weatherURL)
       .then(function (response) {
         return response.json();
       })
       .then(function (data) {
-        // console.log(data);
-        todaysWeatherEl.innerHTML = cityEl.value.toUpperCase() + " | " + moment().format("MMM Do, YYYY");
+        console.log(data)
+        var imgSrc = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
+        currentCityEl.innerHTML = cityEl.value.toUpperCase() + " | " + moment().format("MMM Do, YYYY");
+        currentIconEl.setAttribute("src", imgSrc);
         currentTempEl.innerHTML = "Temp: " + data.main.temp + "°F";
         currentWindEl.innerHTML = "Wind: " + data.wind.speed + "mph";
         currentHumEl.innerHTML = "Humidity: " + data.main.humidity + "%";
@@ -32,26 +36,28 @@ function todaysWeather () {
 // getting the 5 day forecast
 function forecast() {
     var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityEl.value + "&units=imperial" + "&appid=" + APIKey;
-    // event.preventDefault();
 
     fetch(queryURL)
       .then(function (response) {
         return response.json();
       })
       .then(function (data2) {
-        // console.log(data2);
+        forecastHeadingEl.innerHTML = "Five day forecast:"
 
         // iterate through the data 
         for (var i=6; i < data2.list.length; i += 8) {
           var newCard = document.createElement("div");
           newCard.setAttribute("class", "card col-2");
-          var newDate = document.createElement("h4");
+          var newImg = document.createElement("img");
+          var imgSrc = "http://openweathermap.org/img/wn/" + data2.list[i].weather[0].icon + "@2x.png"
           var newTemp = document.createElement("p");
           var newWind = document.createElement("p");
           var newHum = document.createElement("p");
+          newImg.setAttribute("src", imgSrc);
           newTemp.innerHTML = "Temp: " + data2.list[i].main.temp + "°F";
           newWind.innerHTML = "Wind: " + data2.list[i].wind.speed + "mph";
           newHum.innerHTML = "Humidity: " + data2.list[i].main.humidity + "%";
+          newCard.appendChild(newImg);
           newCard.appendChild(newTemp);
           newCard.appendChild(newWind);
           newCard.appendChild(newHum);
@@ -76,12 +82,19 @@ function forecast() {
             newCard.insertBefore(newDate, newTemp);
           } else if (i===38) {
             newDate = document.createElement("h4");
-            newDate.innerHTML = moment().add(2, 'day').format("MMM Do, YYYY"); 
+            newDate.innerHTML = moment().add(5, 'day').format("MMM Do, YYYY"); 
             newCard.insertBefore(newDate, newTemp);
           }
         }
       }   
       )
+}
+// adds button
+function searchHistory() {
+  var histButton = document.createElement("button");
+  histButton.innerHTML = cityEl.value;
+  histButton.setAttribute("class", "btn btn-outline-success w-100");
+  historyEl.appendChild(histButton);
 }
 
 // when the user clicks submit
@@ -89,6 +102,7 @@ function submit(event) {
   event.preventDefault();
   todaysWeather();
   forecast();
+  searchHistory();
 }
 
 searchEl.addEventListener('click', submit);
